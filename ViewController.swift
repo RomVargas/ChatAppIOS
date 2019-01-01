73fd100e5320b66b9548c2f336f5c54b5d3c300e
rom <romvargas81@gmail.com>
2018-12-31 21:17:01 -0600

    scafolding basico de ChatAppIOS

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Roman Vargas on 12/31/18.
+//  Copyright © 2018 Roman Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
